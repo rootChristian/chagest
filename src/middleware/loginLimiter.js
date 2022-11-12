d3df9e3068ead8d1cc147866a525c45a484f9603
rootChagest <softwarechristian1@gmail.com>
2022-11-12 01:14:30 +0100

    Backend chagest

--- src/middleware/loginLimiter.js
+++ src/middleware/loginLimiter.js
@@ -2,10 +2,10 @@ const rateLimit = require('express-rate-limit')
 const { logEvents } = require('./logger')
 
 const loginLimiter = rateLimit({
-    windowMs: 60 * 1000, // 1 minute
+    windowMs: 5 * 60 * 1000, // 5 minutes
     max: 5, // Limit each IP to 5 login requests per `window` per minute
     message:
-        { message: 'Too many login attempts from this IP, please try again after a 60 second pause' },
+        { message: 'Too many login attempts from this IP, please try again after a 5 minutes pause' },
     handler: (req, res, next, options) => {
         logEvents(`Too Many Requests: ${options.message.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errorLog.log')
         res.status(options.statusCode).send(options.message)
